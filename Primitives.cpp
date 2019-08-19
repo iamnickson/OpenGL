145965199b7e9f0d4d31e02027b623bf5ac45501
Anton Jesuthas Nickson Joram <46377224+iamnickson@users.noreply.github.com>
2019-08-19 14:38:43 +0530

    Add files via upload

--- Primitives.cpp
+++ Primitives.cpp
@@ -2,9 +2,8 @@
 
 void init()
 {
-	glClearColor (0.0, 0.0, 0.0, 1.0);
-
-	glColor3f(1.0, 1.0, 1.0); 
+	glClearColor (0.0, 0.0, 0.0, 1.0); //RGB+transparency
+	glColor3f(1.0, 0.5, 0.5); 
 
 	glMatrixMode (GL_PROJECTION);    
 	glLoadIdentity ();    
@@ -13,22 +12,33 @@ void init()
 
 
 void mydisplay(){
-    glClear(GL_COLOR_BUFFER_BIT); 
-	glBegin(GL_POLYGON);        
-		glVertex2f(-0.5, -0.5);        
+    glClear(GL_COLOR_BUFFER_BIT); //specify background colour
+	glPointSize (50);
+	glLineWidth(50);
+	glBegin(GL_POLYGON);   //polygon, points, line, .. primitive type
+	
+	//glBegin(GL_LINES);
+	//glBegin(GL_LINE_LOOP);
+	//glBegin(GL_TRIANGLE_STRIP);
+	//glBegin(GL_QUADS);
+	//GL_QUADS	//glLineWidth(5);
+	//glBegin(GL_POINTS);
+		glVertex2f(-0.5, -0.5);
+		glVertex2f(-0.75, -0.25);
 		glVertex2f(-0.5, 0.5);        
-		glVertex2f(0.5, 0.5);        
-		glVertex2f(0.5, -0.5);    
+		glVertex2f(0.5, 0.5);  
+		glVertex2f(0.1, -0.25); 
+		glVertex2f(0.5, -0.5);    //specify the co-ordinates
 	glEnd();
 	glFlush(); 
 }
 int main(int argc, char** argv){
 	glutInit(&argc, argv);
 	glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB);  
-	glutInitWindowSize(500,500);
-	glutInitWindowPosition(0,0);
-	glutCreateWindow("simple");     
-	glutDisplayFunc(mydisplay);  
-	init();  
-	glutMainLoop();
+	glutInitWindowSize(500,500);	//
+	glutInitWindowPosition(100,100);
+	glutCreateWindow("simple");     //define window properties
+	glutDisplayFunc(mydisplay);  //rendering callback
+	init();  //set Open GL state
+	glutMainLoop(); //enter event loop
 }
