aef981426a38d53e5cbc6e3954e5e55a6d80f6db
Anton Jesuthas Nickson Joram <46377224+iamnickson@users.noreply.github.com>
2019-08-19 13:43:13 +0530

    Add files via upload
    Example given

---
+++ Primitives.cpp
@@ -0,0 +1,34 @@
+#include <GL/glut.h>
+
+void init()
+{
+	glClearColor (0.0, 0.0, 0.0, 1.0);
+
+	glColor3f(1.0, 1.0, 1.0); 
+
+	glMatrixMode (GL_PROJECTION);    
+	glLoadIdentity ();    
+	glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);  
+}
+
+
+void mydisplay(){
+    glClear(GL_COLOR_BUFFER_BIT); 
+	glBegin(GL_POLYGON);        
+		glVertex2f(-0.5, -0.5);        
+		glVertex2f(-0.5, 0.5);        
+		glVertex2f(0.5, 0.5);        
+		glVertex2f(0.5, -0.5);    
+	glEnd();
+	glFlush(); 
+}
+int main(int argc, char** argv){
+	glutInit(&argc, argv);
+	glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB);  
+	glutInitWindowSize(500,500);
+	glutInitWindowPosition(0,0);
+	glutCreateWindow("simple");     
+	glutDisplayFunc(mydisplay);  
+	init();  
+	glutMainLoop();
+}
